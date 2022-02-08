2ba3514a6ce8f59ab8d94b531d8f13cf14a0e410
rishiekumaraarachchi <rishieruween199611@gmail.com>
2022-02-08 22:19:13 +0530

    third commit

--- protected/views/pizza/_form.php
+++ protected/views/pizza/_form.php
@@ -5,26 +5,6 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'pizza_name'); ?>
-		<!-- hii -->
-		<?php echo $form->textField($model,'pizza_name',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'pizza_name'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'pizza_type_id'); ?>
-		<?php echo $form->textField($model,'pizza_type_id'); ?>
-		<?php echo $form->error($model,'pizza_type_id'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
 
 <?php $this->endWidget(); ?>
 
